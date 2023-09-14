564edc0e2dbb9359d5a8e118693073339538cb76
M-Tyshchenko <m.tish2406@gmail.com>
2023-09-14 21:39:00 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,17 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { FormField, FormItem, Wrapper } from './Filter.styled';
 import { changeFilterByName } from 'redux/filterSlice';
+import { getFilterByName } from 'redux/selectors';
 
 export const Filter = () => {
-  const filterByName = useSelector(state => state.filter);
   const dispatch = useDispatch();
+  const filterByName = useSelector(getFilterByName);
+
   return (
     <Wrapper>
       <FormItem>
         Find contacts by name
         <FormField
           type="text"
-          // value={filterByName}
+          value={filterByName}
           onChange={evt => dispatch(changeFilterByName(evt.target.value))}
         />
       </FormItem>
